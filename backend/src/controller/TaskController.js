77095ec1a903d0428af40cfcbce6906ee9d1ff6c
Vinicius Verissimo Viana Bezerra <63198098+Viniverissimo@users.noreply.github.com>
2020-08-05 20:37:56 -0300

    Introdução do PUT
    Criado uma nova rota, PUT, para update de informações já criada

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,5 +1,6 @@
 const TaskModel = require('../model/TaskModel');
 const { response } = require('express');
+const { update } = require('../model/TaskModel');
 
 class TaskController {
 
@@ -8,6 +9,14 @@ class TaskController {
     await task
         .save()
         .then(response => {
+            return res.status(200).json(response);
+        })
+        .catch(error => {
+            return res.status(500).json(error);
+        });
+    }
+    async update(req, res){
+        await TaskModel.findByIdAndUpdate({'_id': req.params.id}, req.body, {new: true}) .then(response => {
             return res.status(200).json(response);
         })
         .catch(error => {
