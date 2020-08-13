211c755367c22332c94c02cb6234751273bb2f21
Vinicius Verissimo Viana Bezerra <63198098+Viniverissimo@users.noreply.github.com>
2020-08-13 08:10:24 -0300

    Criação da rota month
    Criado uma rota para selecionar todas as tarefas que existem no mês

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,5 +1,5 @@
 const TaskModel = require('../model/TaskModel');
-const { startOfDay, endOfDay, startOfWeek, endOfWeek } = require('date-fns');
+const { startOfDay, endOfDay, startOfWeek, endOfWeek, startOfMonth, endOfMonth } = require('date-fns');
 const { response } = require('express');
 const { update } = require('../model/TaskModel');
 const current = new Date();
@@ -113,6 +113,19 @@ class TaskController {
         .catch(error => {
         return res.status(500).json(error);
 });
+    }
+
+    async month(req, res){
+        await TaskModel.find({'macaddress': {'$in': req.body.macaddress},
+        'when': {'$gte': startOfMonth(current), '$lte': endOfMonth(current)}
+})
+        .sort('when')
+        .then(response => {
+            return res.status(200).json(response);
+        })
+        .catch(error =>{
+            return res.status(500).json(error);
+        });
     }
 
 }
