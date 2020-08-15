728fb07484fec5e6f5091f3a2e414276112e20b2
Vinicius Verissimo Viana Bezerra <63198098+Viniverissimo@users.noreply.github.com>
2020-08-14 21:29:10 -0300

    Alteração da porta padrão

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,6 @@ server.use(express.json());
 const TaskRoutes = require('./routes/TaskRoutes');
 server.use('/task', TaskRoutes);
 
-server.listen(3006, () => {
+server.listen(3000, () => {
     console.log('API ONLINE')
 });
