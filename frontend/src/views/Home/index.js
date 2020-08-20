874e2a55ec92bb72f34b3602464e632dc76ba048
Vinicius Verissimo Viana Bezerra <63198098+Viniverissimo@users.noreply.github.com>
2020-08-20 07:51:01 -0300

    Criação do Header
    Criado o Header, atualizando as informações e adicionando a pasta assets

--- frontend/src/views/Home/index.js
+++ frontend/src/views/Home/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import * as S from './styles';
 
 //NOSSOS COMPONENTES
 import Header from '../../components/Header';
 
 function Home() {
-    return <Header/>
+    return (
+    <S.Container>
+        <Header/>
+    </S.Container>
+    )
 }
 
 export default Home;
