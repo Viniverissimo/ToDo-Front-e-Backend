5e3c69a5aa195dfe7794d4081dcc6a436c8538dd
Vinicius Verissimo Viana Bezerra <63198098+Viniverissimo@users.noreply.github.com>
2020-08-24 20:59:50 -0300

    Criação dos containers de tarefa
    Criado containers onde sera exibido as tarefas cadastradas

--- frontend/src/views/Home/styles.js
+++ frontend/src/views/Home/styles.js
@@ -16,5 +16,29 @@ button {
     border: none;
 }
 
+`
+
+export const Content = styled.div `
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    margin-top: 30px;
+    margin-bottom: 200px
+`
+
+export const Title = styled.div `
+    width: 100%;
+    border-bottom: 1px solid #20295F;
+    display: flex;
+    justify-content: center;
+
+    h3 {
+        color: #20295F;
+        position: relative;
+        top: 30px;
+        background: #fff;
+        padding: 0 20px;
+    }
 
 `
